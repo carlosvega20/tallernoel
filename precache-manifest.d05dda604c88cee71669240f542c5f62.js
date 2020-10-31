a6c2482850b12474edc855fc75537dc89daa8ad6
carlosvega20 <carlosvega20@gmail.com>
2020-10-31 01:31:06 +0000

    Deploying to gh-pages from  @ feff4562208a133e26dc5798e5a16d4d91417e20 🚀

---
+++ precache-manifest.d05dda604c88cee71669240f542c5f62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b15f7d3a57122c3aa49e441a9a0ab15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9306f213aa86d0249b9e",
+    "url": "/static/css/main.bb41529b.chunk.css"
+  },
+  {
+    "revision": "7256dc18dc1a30edd165",
+    "url": "/static/js/2.742757b8.chunk.js"
+  },
+  {
+    "revision": "23b26446db404c4cd1d2ae5ba4f80a38",
+    "url": "/static/js/2.742757b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9306f213aa86d0249b9e",
+    "url": "/static/js/main.edd2e4f3.chunk.js"
+  },
+  {
+    "revision": "f50ae17ec83e6577ad20",
+    "url": "/static/js/runtime-main.7c1b040b.js"
+  }
+]);
